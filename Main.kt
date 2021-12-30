184de669234d5750b80dc18b1e13c9acc5a31380
NAPPYLOVE <94218540+NAPPYLOVE@users.noreply.github.com>
2021-12-30 13:41:15 +0300

    Add files via upload

---
+++ Main.kt
@@ -0,0 +1,28 @@
+fun main() {
+
+    val stack = mutableListOf<String>()
+    val ss = readLine()
+    var rght: Boolean = true
+
+    for (i in ss.orEmpty()){
+        if (i in "[{(") {
+            stack.add("$i")
+        }
+
+        if (i in "]})") {
+            if (stack.isEmpty()){
+                rght = false
+                break
+            }
+
+            var opnbr = stack.removeLast()
+            if (opnbr == "(" && i in ")")
+            if (opnbr == "[" && i in "]")
+            if (opnbr == "{" && i in "}")
+            rght = false
+        }
+    }
+
+    if (rght == true && stack.isEmpty()) println("RIGHT") else println("ERROR")
+
+}
